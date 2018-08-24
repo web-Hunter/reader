9dcc1ad7dd63c7ddbafb58052116d5893b887612
Web-Hunter <279646563@qq.com>
2018-08-24 17:45:10 +0800

    header

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <home-header></home-header>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+    name: 'Home',
+    components: {
+        HomeHeader
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="header">
+        <div class="header-input">
+            <span class="header-input-search iconfont">&#xe644;</span>
+            搜索本地或书城
+            <span class="header-input-scan iconfont">&#xe661;</span>
+        </div>
+        <div class="header-right">
+            分类
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+    .header
+        display flex
+        height 1.6rem
+        width 100%
+        margin-top .4rem
+        .header-input
+            flex 1
+            margin-left .56rem
+            font-size .6rem
+            color #999
+            height 1.6rem
+            line-height 1.6rem
+            background rgb(238,238,238)
+            border-radius .3rem 
+            .header-input-search
+                float left
+                margin-left .45rem
+                font-size 1rem
+                margin-right .3rem
+            .header-input-scan
+                float right
+                margin-right .45rem
+        .header-right
+            font-size .6rem
+            width 2.4rem
+            line-height 1.6rem
+            text-align center
+            float right 
+</style>
