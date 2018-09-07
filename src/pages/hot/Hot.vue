4673e86f764ab0a616e15c28cb70461891a769e8
Web-Hunter <279646563@qq.com>
2018-09-07 08:45:21 +0800

    imghot

--- src/pages/hot/Hot.vue
+++ src/pages/hot/Hot.vue
@@ -9,7 +9,7 @@
                     <img src="https://github.com/web-Hunter/reader/blob/master/src/assets/images/hot2.jpg?raw=true" class="swiper-img">
                 </swiper-slide>
                 <swiper-slide>
-                    <img src="" class="swiper-img">
+                    <img src="https://github.com/web-Hunter/reader/blob/master/src/assets/images/hot4.jpg?raw=true" class="swiper-img">
                 </swiper-slide>
                 <div class="swiper-pagination"  slot="pagination"></div>
             </swiper>
